ddb6320a192901621b9e7448f846e5253b471b42
Oliver Kopp <kopp.dev@gmail.com>
2018-06-03 15:19:38 +0200

    Generate demo.pdf during releasing

--- release.bat
+++ release.bat
@@ -1,9 +1,11 @@
 @echo off
 
+rem Generate deml.pdf
+pdflatex demo
+
+rem Generate scientific-thesis-cover.pdf out of README.md
 copy README.md scientific-thesis-cover.md
-
 sed -i "s/.*svg.*//" scientific-thesis-cover.md
-
 pandoc -f markdown_github-hard_line_breaks -t latex scientific-thesis-cover.md -o scientific-thesis-cover.pdf
 
 ctanify --noauto --notds scientific-thesis-cover.sty scientific-thesis-cover.pdf demo.pdf release.bat
